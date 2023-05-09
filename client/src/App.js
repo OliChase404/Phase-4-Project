012e182f5d4bf67ed520e9eca80aaaef7779584d
Cole Perry <colecperry@gmail.com>
2023-05-09 10:54:54 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
+import React from "react";
+import Home from "./Home";
+import Login from "./Login";
+import CreateAccountForm from "./CreateAccountForm";
+import Messages from "./Messages"
+import Profile from "./Profile";
+import { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <Routes>
+        <Route path='/' element ={<Home />} />
+        <Route path='/login' element ={<Login />} />
+        <Route path='/createaccount' element ={<CreateAccountForm />} />
+        <Route path='/profile' element ={<Profile />} />
+        <Route path='/messages' element ={<Messages />} />
+      </Routes>
+      <Login/>
     </div>
   );
 }
