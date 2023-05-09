f7f9b65e2644db9b495f74227a1bb35a35fd2b1f
Cole Perry <colecperry@gmail.com>
2023-05-09 12:22:06 -0700

    Added some login/form components

---
+++ client/src/Login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import CreateAccountForm from "./CreateAccountForm";
+
+function Login() {
+
+
+
+    return (
+    <div>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+            <Button variant="secondary" type="submit">
+                Create Influencer Account
+            </Button>
+            <Button variant="secondary" type="submit">
+                Create Brand Account
+            </Button>
+            </Form>
+            <CreateAccountForm/>
+        </div>
+        );
+    }
+
+export default Login;
