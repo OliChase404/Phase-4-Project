3e757918cfcd9bcee2f9688cadb6c6f6d1f5d499
Oliver <olichase91@gmail.com>
2023-05-09 10:13:49 -0700

    DB MVP

---
+++ server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_migrate import Migrate
+
+from models import db, Influencer, InfluencerCampaign, Campaign, BrandCampaign, Brand
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return '<h1>Hey Man</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -18,22 +18,40 @@ db = SQLAlchemy(metadata=metadata)
 
 class Influencer(db.Model, SerializerMixin):
     __tablename__ = 'influencers'
+    serialize_rules = ('-influencer_campaigns', '-campaigns')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     rank = db.Column(db.Integer)
-    region = db.Column(db.String)
-    language = db.Column(db.String)
     youtube = db.Column(db.String, unique=True)
     twitter = db.Column(db.String, unique=True)
     instagram = db.Column(db.String, unique=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    influencer_campaigns = db.relationship('Campaign', back_populates='influencer')
+    campaigns = association_proxy('influencer_campaigns', 'campaigns', creator=lambda c: InfluencerCampaign(campaigns=c))
+    
+class InfluencerCampaign(db.Model, SerializerMixin):
+    __tablename__ = 'influencer_campaigns'
+    serialize_rules = ('-influencers', '-campaigns')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    influencer_Id = db.Column(db.Integer, db.ForeignKey('influencers.id'))
+    campaign_Id = db.Column(db.Integer, db.ForeignKey('campaigns.id'))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    influencers = db.relationship('Influencer', back_populates='influencer_campaigns')
+    campaigns = db.relationship('Campaign', back_populates='influencer_campaigns')
+    
+    
 
-class Campaigns(db.Model, SerializerMixin):
+class Campaign(db.Model, SerializerMixin):
     __tablename__ = 'campaigns'
+    serialize_rules = ('-influencer_campaigns', '-campaigns')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -41,17 +59,46 @@ class Campaigns(db.Model, SerializerMixin):
     product_category = db.Column(db.String)
     target_revenue = db.Column(db.Integer)
     target_views = db.Column(db.Integer)
-    influencer_id = db.Column(db.Integer, db.ForeignKey('influencers.id'))
     brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    influencer_campaigns = db.relationship('Influencer', back_populates='campaigns')
+    influencers = association_proxy('Influencer_campaigns', 'influencers', creator=lambda i: InfluencerCampaign(influencers=i))
+    
+    brand_campaigns = db.relationship('Brand', back_populates='campaigns')
+    brand = association_proxy('brand_campaigns', 'brands', creator=lambda b: BrandCampaign(brand=b))
+    
+    
+class BrandCampaign(db.Model, SerializerMixin):
+    __tablename__ = 'brand_campaigns'
+    serialize_rules = ('-brand', '-campaign')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'))
+    campaign_id = db.Column(db.Integer, db.ForeignKey('campaigns.id'))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    campaign = db.relationship('Campaign', back_populates='brand_campaigns')
+    brand = db.relationship('Brand', back_populates='brand_campaigns')
+    
 
-class Brands(db.Model)
-    email
-    name
-    password
-    cate
 
+class Brand(db.Model):
+    __tablename__ = 'brands'
+    serialize_rules = ('-brand_campaign', '-campaign')
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    brand_name = db.Column(db.String)
+    password = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    campaigns = db.relationship('Campaign', back_populates='brands')
+    brand_campaigns = db.relationship('Campaign', back_populates='brand_campaigns')
+    
 
     
 
