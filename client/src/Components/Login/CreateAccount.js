c360b64532718680265b20f423d685291f2fc150
Oliver <olichase91@gmail.com>
2023-05-10 09:45:02 -0700

    Component Folders

---
+++ client/src/Components/Login/CreateAccount.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from "react";
+import InfluencerForm from "./InfluencerForm";
+import BrandForm from "./BrandForm";
+
+
+
+function CreateAccount() {
+    const [isOn, setIsOn] = useState(false);
+    const [switchForm, setSwitchForm] = useState(true);
+
+    const handleClick1 = () => {
+        setIsOn(!isOn)
+    };
+
+    const handleClick2 = () => {
+        setSwitchForm(!switchForm)
+    }
+
+
+    return (
+        <div className="CreateAccountForm">
+        {/* <h1 style={{paddingTop:"50px", paddingBottom:"50px", fontSize:"3.5rem"}}>SPHERE</h1>
+        <button style={{backgroundColor: `${isOn ? '#D3D3D3' : '#808080'}`}} onClick={() => {handleClick1(), handleClick2()}}>
+            Create an Influencer Account
+        </button>
+        <button style={!isOn ? { backgroundColor: '#D3D3D3'} : { backgroundColor: '#808080'}} onClick={() => {handleClick1(), handleClick2()}}>
+            Create a Brand Account
+        </button>
+        {switchForm ? <InfluencerForm/> : <BrandForm/>} */}
+        </div>
+    )
+}
+
+export default CreateAccount;
---
+++ client/src/Components/Login/Login.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {Link} from "react-router-dom"
+
+function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+    let credentials = {
+        email: email,
+        password: password
+    }
+
+    const response = await fetch('proxy', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+        })
+        .then(resp => resp.json())
+    };
+
+
+    return (
+    <div>
+        <h1 style={{paddingTop:"50px", paddingBottom:"50px", fontSize:"3.5rem"}}>SPHERE</h1>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+            </Form.Group>
+            <Link to='/home'>
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+            </Link>
+                &nbsp;
+            <Link to='/createaccount'>
+                <Button variant="secondary" type="button">
+                    Create Account
+                </Button>
+            </Link>
+        </Form>
+    </div>
+        );
+    }
+
+export default Login;
