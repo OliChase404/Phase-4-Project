25467c38d9a6c55bb0920e9f451249151e5f5b7d
Cole Perry <colecperry@gmail.com>
2023-09-22 16:14:34 -0700

    Add semantic

--- client/src/Components/Home/BrandCampaignCard.js
+++ client/src/Components/Home/BrandCampaignCard.js
@@ -1,27 +1,19 @@
 import React, {useState, useEffect} from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Container, Divider } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
 
 function BrandCampaignCard({campaign, user}) {
 
 
+
     return(
         <div>
-          <Container style={{ 
-            textAlign: 'justified', 
-            backgroundColor: '#f8f8f8', 
-            padding: '20px', 
-            borderRadius: '5px', 
-            boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.3)' 
-          }}>
-            <b>Campaign Name:</b>{campaign.name}
-            <Divider />
-            <b>Product Category: </b> {campaign.product_category}
-            <b>Budget: </b> ${campaign.budget}
-            <b>Target Revenue: </b>${campaign.target_revenue}
-            <b>Target Views </b>{campaign.target_views}
-          </Container>
+          <Card.Group>
+            <Card fluid color='black' header= {'Campaign Name: ' + campaign.name} />
+            <Card fluid color='black' header= {'Product Category: ' + campaign.product_category} />
+            <Card fluid color='black' header= {'Budget: ' + campaign.budget} />
+            <Card fluid color='black' header= {'Target Revenue: ' + campaign.target_revenue} />
+            <Card fluid color='black' header= {'Target Views: ' + campaign.target_views} />
+          </Card.Group>
         </div>
     )
 }
